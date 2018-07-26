372a1735ec2037aa52864b9b24e8fca7ddec87cf
chrisho <cenne1986@qq.com>
2018-07-26 15:46:32 +0800

    修改setup.py信息

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="grpc-py",
+    name="grpcpy",
     version="0.0.1",
     author="Chris Ho",
     author_email="cenne1986@qq.com",
     description="easy to use tls for grpc",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/chrisho/grpc-py",
+    url="https://github.com/chrisho/grpcpy",
     packages=setuptools.find_packages(),
     classifiers=(
         "Programming Language :: Python :: 3",
